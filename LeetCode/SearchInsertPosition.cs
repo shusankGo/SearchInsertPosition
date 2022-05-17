14673c7739710acdf05a2221b0d28f70dc86fe61
shusankgo <shushank.dahal@gokloud.io>
2022-05-17 15:20:55 +0545

    Added the solution of Search Insert Position program

---
+++ LeetCode/SearchInsertPosition.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode
+{
+    public class SearchInsertPosition
+    {
+        public static int PositionSearch(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                    return i;
+            }
+            return nums.Length;
+        }
+    }
+}
---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode;
+using Xunit;
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new int[] { 0, 3, 7, 6 }, 2, 1)]
+        [InlineData(new int[] { 1, 2 }, 2, 1)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 6, 5)]
+        [InlineData(new int[] { 2, 4, 6, 8, 10 }, 6, 2)]
+        [InlineData(new int[] { 10, 15, 22, 33 }, 9, 0)]
+        public void SearchInsertPositionTest(int[] nums, int target, int expected)
+        {
+            var actual = SearchInsertPosition.PositionSearch(nums, target);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
+
